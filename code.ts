6dcf0e52657559abffb994520167c3f320252b63
Jakub Žitný <jakubzitny@avocode.com>
2020-09-17 02:33:17 +0200

    count number of frames and pages in a Figma file

--- code.ts
+++ code.ts
@@ -1,2 +1,12 @@
 
-figma.closePlugin('Hello from Figma 22 🎨')
+
+const pageCount = figma.root.children.length
+const frameCount = figma.root.children.reduce((count: number, page: PageNode) => {
+  const frames = page.children.filter((pageChild: SceneNode) => {
+    return pageChild.type === 'FRAME'
+  })
+
+  return count + frames.length
+}, 0)
+
+figma.closePlugin(`There are ${pageCount} pages and ${frameCount} artboards 🎨`)
